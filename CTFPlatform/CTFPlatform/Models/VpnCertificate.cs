1d99f7e60b9175307ec064c9534d6cbbf3225316
Chelsea Pritchard <chelsea@dotefekts.net>
2025-06-27 20:04:44 +0800

    Fix css. rename migration.

--- CTFPlatform/CTFPlatform/Models/VpnCertificate.cs
+++ CTFPlatform/CTFPlatform/Models/VpnCertificate.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CTFPlatform.Models;
 
 public class VpnCertificate
 {
     public int Id { get; set; }
+    [Required]
     public string Certificate { get; set; }
     public DateTime Expiry { get; set; }
     public bool Valid { get; set; }
